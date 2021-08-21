29898dea03076c4d4cde5c83d6096a2edb24e3b8
Zak <89150707+Zak0749@users.noreply.github.com>
2021-08-21 23:16:29 +0100

    doker fixes

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,1 +1,2 @@
 docker run --rm --name nginx-proxy -p 443:443 -v "/Users/zakg/quiz-app/nginx/certs" -v "/Users/zakg/quiz-app/nginx:/etc/nginx" nginx:latest &
+docker run --rm --name nginx-proxy -p 443:443 -v "/Users/zakg/quiz-app/nginx/certs" -v "/Users/zakg/quiz-app/nginx:/etc/nginx" nginx:latest &

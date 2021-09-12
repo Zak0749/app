e3b095635fad4922e9f3137e50dd12518a330279
Zak <89150707+Zak0749@users.noreply.github.com>
2021-09-12 23:15:28 +0100

    Login View

--- src/client/pages/LoginView.tsx
+++ src/client/pages/LoginView.tsx
@@ -1,24 +1,35 @@
-import { Form, Modal, Button } from 'react-bootstrap';
-// import { Formik } from 'formik';
-// import * as yup from 'yup';
-import { Check } from 'react-bootstrap-icons';
+import {
+  Form, Modal, Button, FloatingLabel,
+} from 'react-bootstrap';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+import { useContext, useEffect } from 'react';
+import { axios } from '../helpers/axios';
+import loggedInContext from '../helpers/logged-in-context';
+import modalContext from '../helpers/modal-context';
+import SignUpView from './SignUpView';
 
-// const schema = yup.object().shape({
-//   username: yup
-//     .string()
-//     .required()
-//     .matches(RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*d)[a-zA-Zd]$'))
-//     .min(1)
-//     .max(32),
-//   password: yup
-//     .string()
-//     .required()
-//     .matches(RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*d)[a-zA-Zd]$'))
-//     .min(8)
-//     .max(32),
-// });
+const schema = yup.object().shape({
+  username: yup
+    .string()
+    .required()
+    .min(1)
+    .max(32),
+  password: yup
+    .string()
+    .required()
+    .min(1)
+    .max(32),
+});
 
 function LoginView() {
+  const { refresh } = useContext(loggedInContext);
+  const [modal, setModal] = useContext(modalContext);
+
+  useEffect(() => {
+    // set modal to session view :)
+  });
+
   return (
     <Modal.Dialog style={{
       maxWidth: '700px', width: '90%', height: '90%',
@@ -28,11 +39,25 @@ function LoginView() {
         <Modal.Title id="example-custom-modal-styling-title">
           Login
         </Modal.Title>
+
+        <Button variant="info" className="ms-3" onClick={() => setModal({ show: true, element: SignUpView })}>Signup</Button>
       </Modal.Header>
       <Modal.Body>
-        {/* <Formik
+        <Formik
           validationSchema={schema}
-          onSubmit={console.log}
+          onSubmit={async (values, set) => {
+            try {
+              await axios({
+                url: '/session',
+                method: 'GET',
+                data: values,
+              });
+              refresh();
+              setModal({ show: false, element: modal.element });
+            } catch (err) {
+              set.setErrors({ username: '  ', password: 'Your email or password was incorrent please check the feilds and try again' });
+            }
+          }}
           initialValues={{
             username: '',
             password: '',
@@ -41,41 +66,47 @@ function LoginView() {
           {({
             handleSubmit,
             handleChange,
-            handleBlur,
             values,
             touched,
-            isValid,
             errors,
           }) => (
             <Form noValidate onSubmit={handleSubmit}>
-              <Form.Group controlId="validationFormik01">
-                <Form.Label>First name</Form.Label>
+              <FloatingLabel label="Username" className="mb-3" controlId="validationFormik01">
                 <Form.Control
                   type="text"
                   name="username"
+                  placeholder="Username"
                   value={values.username}
                   onChange={handleChange}
                   isValid={touched.username && !errors.username}
+                  isInvalid={!!errors.username}
                 />
-                <Form.Control.Feedback><Check color="green" /></Form.Control.Feedback>
-              </Form.Group>
 
-              <Form.Group controlId="validationFormik02">
-                <Form.Label>First name</Form.Label>
+                <Form.Control.Feedback type="invalid">
+                  {errors.username}
+                </Form.Control.Feedback>
+              </FloatingLabel>
+
+              <FloatingLabel label="Password" className="mb-3" controlId="validationFormik02">
                 <Form.Control
                   type="password"
                   name="password"
+                  placeholder="Password"
                   value={values.password}
                   onChange={handleChange}
                   isValid={touched.password && !errors.password}
+                  isInvalid={!!errors.password}
                 />
-                <Form.Control.Feedback><Check color="green" /></Form.Control.Feedback>
-              </Form.Group>
 
-              <Button type="submit">Submit form</Button>
+                <Form.Control.Feedback type="invalid">
+                  {errors.password}
+                </Form.Control.Feedback>
+              </FloatingLabel>
+
+              <Button type="submit">Submit</Button>
             </Form>
           )}
-        </Formik> */}
+        </Formik>
       </Modal.Body>
     </Modal.Dialog>
   );
